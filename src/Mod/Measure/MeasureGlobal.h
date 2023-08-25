c8d77903ca5ab9c8e13ebf0f64a689a12e721f95
hlorus <david@friedli-be.ch>
2023-08-25 18:41:22 +0200

    Measure: Define MeasureGuiExport

--- src/Mod/Measure/MeasureGlobal.h
+++ src/Mod/Measure/MeasureGlobal.h
@@ -35,4 +35,13 @@
 #endif
 #endif
 
+// MeasureGui
+#ifndef MeasureGuiExport
+#ifdef MeasureGui_EXPORTS
+# define MeasureGuiExport   FREECAD_DECL_EXPORT
+#else
+# define MeasureGuiExport   FREECAD_DECL_IMPORT
+#endif
+#endif
+
 #endif //MEASURE_GLOBAL_H
